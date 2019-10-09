549f3144ee876fb43c6fe61c7c2f6bd3c3213be5
Guillaume Haben <pyxouze@gmail.com>
2019-10-09 11:27:47 +0200

    Create model from Java project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
+import spoon.Launcher;
+import spoon.reflect.CtModel;
 
 public class Main {
 
     public static void main(String [] args){
 
+        // Input project - folder path
+        String projectPath = "/Users/guillaume.haben/Documents/Work/projects/TooTallNate/Java-WebSocket/";
+
+        // Creating launcher
+        Launcher launcher = new Launcher();
+        launcher.addInputResource(projectPath);
+
+        // Creating model
+        launcher.buildModel();
+        CtModel model = launcher.getModel();
+
+        System.out.println("Analyzing project: " + projectPath);
 
     }
 }
